b3172be33c214fc742b0914135cff8216fbe3a13
Josh Hornby <josh.hornby@fastwebmedia.com>
2014-06-28 17:44:49 +0100

    Added post, delete, put, head request types

--- src/JoshHornby/Http/HttpCore.php
+++ src/JoshHornby/Http/HttpCore.php
@@ -23,5 +23,73 @@ class HttpCore {
 
         throw new HttpRequestNotFound($response->getStatusCode().' status code returned');
     }
+    
+    /**
+     * @param $request
+     * @return mixed
+     * @throws \SmokeSpots\HTTP\Exceptions\HttpRequestNotFound
+     */
+    public static function post($request)
+    {
+        $client = new Client();
+        $response = $client->post(strtolower($request));
+
+        if ($response->getStatusCode() == '200') {
+            return $response->json();
+        }
+
+        throw new HttpRequestNotFound($response->getStatusCode().' status code returned');
+    }
+    
+    /**
+     * @param $request
+     * @return mixed
+     * @throws \SmokeSpots\HTTP\Exceptions\HttpRequestNotFound
+     */
+    public static function put($request)
+    {
+        $client = new Client();
+        $response = $client->put(strtolower($request));
+
+        if ($response->getStatusCode() == '200') {
+            return $response->json();
+        }
+
+        throw new HttpRequestNotFound($response->getStatusCode().' status code returned');
+    }
+    
+    /**
+     * @param $request
+     * @return mixed
+     * @throws \SmokeSpots\HTTP\Exceptions\HttpRequestNotFound
+     */
+    public static function delete($request)
+    {
+        $client = new Client();
+        $response = $client->delete(strtolower($request));
+
+        if ($response->getStatusCode() == '200') {
+            return $response->json();
+        }
+
+        throw new HttpRequestNotFound($response->getStatusCode().' status code returned');
+    }
+    
+    /**
+     * @param $request
+     * @return mixed
+     * @throws \SmokeSpots\HTTP\Exceptions\HttpRequestNotFound
+     */
+    public static function head($request)
+    {
+        $client = new Client();
+        $response = $client->head(strtolower($request));
+
+        if ($response->getStatusCode() == '200') {
+            return $response->json();
+        }
+
+        throw new HttpRequestNotFound($response->getStatusCode().' status code returned');
+    }
 
 } 
